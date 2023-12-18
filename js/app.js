b25b01148207c22a6784c2431268aff1a2f73f99
hasan ashrafi <92205365+hasanashrafi@users.noreply.github.com>
2023-12-18 11:39:13 +0330

    refactor codes app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5/"
 const API_KEY = "3622d22286024a6a008e0983c93e34ba"
-
+const DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
+const forecastContainer = document.getElementById("forecast");
 const locationIcon = document.getElementById("location")
 
+const getWeatherData = async (type, data) => {
+    let url = null;
+    switch (type) {
+        case "current":
+            if (typeof data === "string") {
+                url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
+            } else {
+                url = `${BASE_URL}/weather?lat=${data.lat}&lon=${data.lon}&appid=${API_KEY}&units=metric`
+            }
+            break;
+        case "forecast":
+            if (typeof data === "string") {
+                url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric`
+            } else {
+                url = `${BASE_URL}/forecast?lat=${data.lat}&lon=${data.lon}&appid=${API_KEY}&units=metric`
+            } forecast
+            break;
+
+        default:
+            url = `${BASE_URL}/weather?q=tehran&appid=${API_KEY}&units=metric`
+            break;
+    }
+    const response = await fetch(url);
+    const json = await response.json();
+    return json
+}
 
 const getCurrentWeatherByName = async (city) => {
     const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
@@ -28,6 +63,13 @@ const getForecastWeatherByName = async (city) => {
     return json
 }
 
+const getForecastWeatherByCoordinates = async (lat, lon) => {
+    const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const json = await response.json();
+    return json
+}
+
 const renderCurrentWeather = (data) => {
     console.log(data);
     const weatherJSX = `
@@ -45,6 +87,25 @@ const renderCurrentWeather = (data) => {
     weatherContainer.innerHTML = weatherJSX
 }
 
+const getWeekDay = (date) => {
+    return DAYS[new Date(date * 1000).getDay()];
+}
+const renderForecastWeather = (data) => {
+    forecastContainer.innerHTML = "";
+    data = data.list.filter((obj => obj.dt_txt.endsWith("12:00:00")))
+    data.forEach((i) => {
+        const forecastJSX = `
+        <div>
+            <img alt="weather icon" src="https://openweathermap.org/img/w/${i.weather[0].icon}.png"/>
+            <h3>${getWeekDay(i.dt)}</h3>
+            <p>${Math.round(i.main.temp)} °C</p>
+            <span>${i.weather[0].main}</span>
+        </div>
+        `
+        forecastContainer.innerHTML += forecastJSX
+    });
+}
+
 const searchHandler = async () => {
     const cityName = searchInput.value
     if (!cityName) {
@@ -54,13 +115,15 @@ const searchHandler = async () => {
     const currentData = await getCurrentWeatherByName(cityName)
     renderCurrentWeather(currentData)
     const forecastData = await getForecastWeatherByName(cityName);
-    console.log(forecastData);
+    renderForecastWeather(forecastData)
 }
 
 const positionCallback = async (position) => {
     const { latitude, longitude } = position.coords
     const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
     renderCurrentWeather(currentData)
+    const forecastData = await getForecastWeatherByCoordinates(latitude, longitude)
+    renderForecastWeather(forecastData)
 }
 
 
